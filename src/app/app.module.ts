4fe59b2a207457158a7b51992719ab64afec6980
aditi <aditi.fbg@gmail.com>
2023-07-28 08:03:46 +0530

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule,
