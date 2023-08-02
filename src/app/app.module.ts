9868b1f4650ad592d778b4b8248dcd2479b7662b
aditi <aditi.fbg@gmail.com>
2023-08-02 09:01:10 +0530

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HeaderComponent } from './header/header.component';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +23,8 @@ import { SubHeaderComponent } from './sub-header/sub-header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
